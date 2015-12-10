62be6f91a106cbbebf4dfbc109b4a45f2e0bf059
luz <luzma_87@yahoo.com>
2015-12-09 22:43:04 -0500

    added bank style, item icons, item descriptions

---
+++ js/item_descriptions.js
@@ -0,0 +1,46 @@
+var items = {
+    "hearthstone"          : {
+        "icon"   : "images/items/hearthstone.jpg",
+        "name"   : "Hearthstone",
+        "level"  : 1,
+        "bop"    : "Binds when picked up",
+        "unique" : true,
+        "use"    : "Returns you to [Hearthstone Location].  Speak to an Innkeeper in a different place to change your home location. (30 Min Cooldown)"
+    },
+    "garrison_hearthstone" : {
+        "icon"     : "images/items/garrison_hearthstone.jpg",
+        "name"     : "Garrison Hearthstone",
+        "level"    : 90,
+        "bop"      : "Binds when picked up",
+        "unique"   : true,
+        "use"      : "Returns you to your Garrison. (20 Min Cooldown)",
+        "requires" : "Level 90"
+    },
+    "dragonhide_bag"       : {
+        "icon"   : "images/bags/dragonhide_22.jpg",
+        "name"   : "Dragon Hide Bag",
+        "level"  : 80,
+        "unique" : true,
+        "slots"  : 22
+    },
+    "gronnskin_bag"        : {
+        "icon"   : "images/bags/gronnskin_28.jpg",
+        "name"   : "Gronnskin Bag",
+        "level"  : 90,
+        "unique" : true,
+        "slots"  : 28
+    },
+    "woolen_bag"           : {
+        "icon"  : "images/bags/red_woolen_8.jpg",
+        "name"  : "Red Woolen Bag",
+        "level" : 15,
+        "slots" : 8
+    },
+    "zoes_cape"            : {
+        "icon"     : "images/items/zoes_cape.jpg",
+        "name"     : "Zoe's Comforting Cape",
+        "level"    : 150,
+        "bop"      : "Binds when equipped",
+        "requires" : "Level 74"
+    }
+};
